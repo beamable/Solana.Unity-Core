bf9c05194f6b286159b2a1cd432b0b2d72029424
Drazen Patekar <dpatekar@gmail.com>
2023-01-19 09:52:48 +0100

    Remove Unity dependencies

--- src/Solana.Unity.Rpc/Core/Http/CrossHttpClient.cs
+++ src/Solana.Unity.Rpc/Core/Http/CrossHttpClient.cs
@@ -4,7 +4,6 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine.Networking;
 
 namespace Solana.Unity.Rpc.Core.Http;
 
@@ -21,41 +20,6 @@ public static class CrossHttpClient
     /// <returns></returns>
     public static async Task<HttpResponseMessage> SendAsyncRequest(HttpClient httpClient, HttpRequestMessage httpReq)
     {
-        if (RuntimePlatform.IsUnityPlayer())
-        {
-            return await SendUnityWebRequest(httpClient.BaseAddress != null ? 
-                httpClient.BaseAddress: httpReq.RequestUri, httpReq);
-        }
         return await httpClient.SendAsync(httpReq).ConfigureAwait(false);
-    }
-        
-    /// <summary>
-    /// Convert a httReq to a Unity Web request
-    /// </summary>
-    /// <param name="uri">RPC URI</param> 
-    /// <param name="httpReq">The http request</param>
-    /// <returns>Http response</returns>
-    /// <exception cref="HttpRequestException"></exception>
-    private static async Task<HttpResponseMessage> SendUnityWebRequest(Uri uri, HttpRequestMessage httpReq)
-    {
-        Byte[] buffer = await httpReq.Content.ReadAsByteArrayAsync();
-        using (var request = new UnityWebRequest(uri, httpReq.Method.ToString()))
-        {
-            request.uploadHandler = new UploadHandlerRaw(buffer);
-            request.downloadHandler = new DownloadHandlerBuffer();
-            request.SetRequestHeader("Content-Type", "application/json");
-            request.SendWebRequest();
-            if (request.result == UnityWebRequest.Result.ConnectionError)
-            {
-                throw new HttpRequestException("Error While Sending: " + request.error);
-            }
-            while (!request.isDone)
-            {
-                await Task.Yield();
-            }
-            var response = new HttpResponseMessage(HttpStatusCode.OK);
-            response.Content = new ByteArrayContent(Encoding.UTF8.GetBytes(request.downloadHandler.text));
-            return response;
-        }
     }
 }
--- src/Solana.Unity.Rpc/Core/Http/JsonRpcClient.cs
+++ src/Solana.Unity.Rpc/Core/Http/JsonRpcClient.cs
@@ -10,7 +10,6 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine.Networking;
 
 namespace Solana.Unity.Rpc.Core.Http
 {
